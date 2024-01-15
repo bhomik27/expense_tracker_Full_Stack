73d57f6706fb086f28fb7ab0585e786a6ccc8e4c
bhomik27 <bhomikmaheshwari27@gmail.com>
2024-01-15 06:28:18 +0530

    deleted the file due to exposure of api key, added environment variable to use in place of apikey

--- controllers/password.js
+++ controllers/password.js
@@ -1,11 +1,13 @@
+// Load environment variables from .env file
+require('dotenv').config();
 
 exports.getresetPassword = async (req, res) => {
-    const useremail=req.body.email
+    const useremail = req.body.email;
     var Sib = require('sib-api-v3-sdk');
     var Client = Sib.ApiClient.instance;
     
     var apiKey = Client.authentications['api-key'];
-    apiKey.apiKey = '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';
+    apiKey.apiKey = process.env.SIB_API_KEY; // Use the environment variable
 
     const transEmailApi = new Sib.TransactionalEmailsApi();
 
